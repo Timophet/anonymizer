75aad08b57fef47708fe7b3c21b9b73ffc63676f
Timophet <timophet@yandex.ru>
2017-07-09 14:24:27 +0800

    1

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
+echo phpinfo();
 echo"<pre>";
 echo"<pre>" ;
-var_dump($_GET);
+//var_dump($_GET);
 echo "</pre>";
 
 $data_url = parse_url($_GET['url']);
@@ -19,7 +20,7 @@ else
 	$data_url['query']='';
 
 echo"<pre>" ;
-var_dump($data_url);
+//var_dump($data_url);
 echo "</pre>";
 
 if($data_url['scheme'] == 'http')
@@ -33,11 +34,11 @@ else
 	$port = 443;
 }
 $protocol .= $data_url['host'];
-echo"$protocol";
+//echo"$protocol";
 
 $fp = fsockopen($protocol, $port);
 echo"<pre>" ;
-	var_dump($_SERVER);
+	//var_dump($_SERVER);
 echo "</pre>";
 
 if(!$fp)
@@ -46,6 +47,29 @@ if(!$fp)
 }
 $request = $_SERVER["REQUEST_METHOD"];
 $server_protocol = $_SERVER["SERVER_PROTOCOL"];
-$out = "$request / $server_protocol" . PHP_EOL;
-	//$out .= 
+$path = $data_url['path'];
+$query = $data_url['query'];
+$out = "$request $path$query $server_protocol" . PHP_EOL;
+$out .= "Host: $data_url[host]" . PHP_EOL;
+$out .= "User agent: $_SERVER[HTTP_USER_AGENT]" . PHP_EOL;
+$out .= "Connection: close" . PHP_EOL . PHP_EOL;
+//$out .= PHP_EOL;
 echo "$out";
+//echo"<hr>";
+	//$out .= 
+//$out ="";	
+fwrite($fp, $out);
+	
+while (!feof($fp))
+{
+	$body .= fgets($fp, 128);
+}	
+fclose($fp);
+list($header, $body) = explode(PHP_EOL . PHP_EOL, $body);
+
+//$header = http_parse_headers($header);
+echo "$header";
+echo"<hr>";
+echo "$body";
+
+//var_dump($out);
