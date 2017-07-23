0fe1e94b8c488db6f64058e4ef81cca6c9eb89f7
Timophet <timophet@yandex.ru>
2017-07-23 14:22:38 +0800

    1

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
+require_once"lib\parse_headers.php";
+//require_once"lib\encode.php";
+//echo phpinfo();
 
-echo phpinfo();
-echo"<pre>";
-echo"<pre>" ;
-//var_dump($_GET);
-echo "</pre>";
+
+
 
 $data_url = parse_url($_GET['url']);
-echo"<pre>" ;
-var_dump($data_url);
-echo "</pre>";
+
+if (count($data_url) < 2)
+	die("Wrong url in request query");
 
 if(!$data_url['path'])
 	$data_url['path']='/';
@@ -19,9 +19,7 @@ if($data_url['query'])
 else
 	$data_url['query']='';
 
-echo"<pre>" ;
-//var_dump($data_url);
-echo "</pre>";
+
 
 if($data_url['scheme'] == 'http')
 {
@@ -37,9 +35,7 @@ $protocol .= $data_url['host'];
 //echo"$protocol";
 
 $fp = fsockopen($protocol, $port);
-echo"<pre>" ;
-	//var_dump($_SERVER);
-echo "</pre>";
+
 
 if(!$fp)
 {
@@ -54,12 +50,13 @@ $out .= "Host: $data_url[host]" . PHP_EOL;
 $out .= "User agent: $_SERVER[HTTP_USER_AGENT]" . PHP_EOL;
 $out .= "Connection: close" . PHP_EOL . PHP_EOL;
 //$out .= PHP_EOL;
-echo "$out";
+
+//echo "$out";
 //echo"<hr>";
 	//$out .= 
 //$out ="";	
 fwrite($fp, $out);
-	
+$body='';	
 while (!feof($fp))
 {
 	$body .= fgets($fp, 128);
@@ -67,9 +64,48 @@ while (!feof($fp))
 fclose($fp);
 list($header, $body) = explode(PHP_EOL . PHP_EOL, $body);
 
-//$header = http_parse_headers($header);
-echo "$header";
-echo"<hr>";
-echo "$body";
 
-//var_dump($out);
+$header = http_parse_headers($header);
+
+list($content_type, $content_charset) = explode(";", $header['Content-Type']);
+if($content_type == 'text/html')
+{
+	//echo "$body";
+
+
+	libxml_use_internal_errors(true);
+	$html = new DOMDocument();
+	$html->loadHTML($body);
+	$html_resource = array(
+		"img" => "src",
+		"input" => "src",
+		"script" => "scr",
+		"link" => "href",
+		"a" => "href",
+		"form" => "action"
+	);
+
+
+	foreach ($html_resource as $tag => $attr)
+	{
+		foreach($html->getElementsByTagName($tag) as $element)
+		{
+			if($element->hasAttribute($attr))
+			{
+				//var_dump($element->getAttribute($attr));
+				//echo " - ";
+				$element->setAttribute($attr,  encode_resource($element->getAttribute($attr)));
+				//echo "<br>";
+			}
+		}
+	}
+	$body = $html->saveHTML();
+	$body = str_replace("parent&&parent!=window&&(document.getElementsByTagName('body')[0].innerHTML='');", "", $body);
+	$body = str_replace("location.replace(location.toString())", "", $body);
+	
+	echo "$body";
+};
+
+;
+	
+//if()
